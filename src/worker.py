947f6c4734af1d14fe49f9ae88c5918c9be31e82
Damian Birchler <damian@damianbirchler.net>
2025-04-22 00:02:19 +0200

    Add workflow for creating OpenStack image.

--- src/worker.py
+++ src/worker.py
@@ -1,6 +1,12 @@
+import os
+import pathlib as pl
+
 import workflow as wf
 
 if __name__ == "__main__":
+    containing_dir_path = pl.Path(__file__).parent
+    os.chdir(containing_dir_path)
+    
     wf.retrieve_github_stars.serve(
         parameters={
             "repos": ["python/cpython", "prefectHQ/prefect"],
