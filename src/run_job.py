91d95bd2d496e62023e34325c90f69dcbe8bf4e3
Damian Birchler <damian@damianbirchler.net>
2025-09-03 15:37:25 +0200

    Update `pydantic-models` and unify logs.

--- src/run_job.py
+++ src/run_job.py
@@ -47,7 +47,7 @@ def _get_result_paths(
 async def run_job(
     context: _con.Context, runner_job: _mrunner.RunnerJob
 ) -> _jrpcs.Result:
-    _LOGGER.info("%s - Starting job.", runner_job)
+    _LOGGER.info("%s - Starting job.", runner_job.id)
 
     object_storage_path = runner_job.object_storage_path
 
